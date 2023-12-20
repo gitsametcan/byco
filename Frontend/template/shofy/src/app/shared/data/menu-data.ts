e9f05134d8018998ab782ebf7bc30d477bed946b
Kadir Berk Yağar <ykadirberk2@gmail.com>
2023-12-21 00:38:10 +0300

    Fixing header and anasayfa

--- Frontend/template/shofy/src/app/shared/data/menu-data.ts
+++ Frontend/template/shofy/src/app/shared/data/menu-data.ts
@@ -3,9 +3,9 @@ import { IMenuItem, IMobileType } from "@/types/menu-d-type";
 export const menu_data:IMenuItem[] = [
   {
     id:1,
-    link:'/home',
+    link:'/anasayfa',
     title:'Anasayfa',
-    mega_menu:true,
+    mega_menu:false,
     // home_pages:[
     //   {
     //     id:1,
@@ -37,106 +37,106 @@ export const menu_data:IMenuItem[] = [
     id:2,
     link:'/shop',
     title:'Ticaret',
-    mega_menu:true,
-    shop_mega_menus:[
-      {
-        link:'/shop',
-        title:'Ticaret Sayfaları',
-        list_menus:[
-          {title:'Grid Layout',link:'/shop'},
-          {title:'Shop Categories',link:'/shop/shop-category'},
-          {title:'List Layout',link:'/shop/shop-list'},
-          {title:'Full width Layout',link:'/shop/shop-full-width'},
-          {title:'1600px Layout',link:'/shop/shop-1600'},
-          {title:'Left Sidebar',link:'/shop'},
-          {title:'Right Sidebar',link:'/shop/shop-right-sidebar'},
-          {title:'Hidden Sidebar',link:'/shop/shop-no-sidebar'},
-        ]
-      },
-      {
-        link:'/shop',
-        title:'Özellikler',
-        list_menus:[
-          {title:'Filter Dropdown',link:'/shop/shop-filter-dropdown'},
-          {title:'Filters Offcanvas',link:'/shop/shop-filter-offcanvas'},
-          {title:'Filters Sidebar',link:'/shop'},
-          {title:'Load More button',link:'/shop/shop-load-more'},
-          {title:'1600px Layout',link:'/shop/shop-1600'},
-          {title:'Collections list',link:'/shop/shop-list'},
-          {title:'Hidden search',link:'/shop'},
-          {title:'Search Full screen',link:'/shop'},
-        ]
-      },
-      {
-        link:'/shop',
-        title:'Hover Style',
-        list_menus:[
-          {title:'Hover Style 1',link:'/shop'},
-          {title:'Hover Style 2',link:'/shop'},
-          {title:'Hover Style 3',link:'/shop'},
-          {title:'Hover Style 4',link:'/shop'}
-        ]
-      },
-    ]
+    mega_menu:false,
+    // shop_mega_menus:[
+    //   {
+    //     link:'/shop',
+    //     title:'Ticaret Sayfaları',
+    //     list_menus:[
+    //       {title:'Grid Layout',link:'/shop'},
+    //       {title:'Shop Categories',link:'/shop/shop-category'},
+    //       {title:'List Layout',link:'/shop/shop-list'},
+    //       {title:'Full width Layout',link:'/shop/shop-full-width'},
+    //       {title:'1600px Layout',link:'/shop/shop-1600'},
+    //       {title:'Left Sidebar',link:'/shop'},
+    //       {title:'Right Sidebar',link:'/shop/shop-right-sidebar'},
+    //       {title:'Hidden Sidebar',link:'/shop/shop-no-sidebar'},
+    //     ]
+    //   },
+    //   {
+    //     link:'/shop',
+    //     title:'Özellikler',
+    //     list_menus:[
+    //       {title:'Filter Dropdown',link:'/shop/shop-filter-dropdown'},
+    //       {title:'Filters Offcanvas',link:'/shop/shop-filter-offcanvas'},
+    //       {title:'Filters Sidebar',link:'/shop'},
+    //       {title:'Load More button',link:'/shop/shop-load-more'},
+    //       {title:'1600px Layout',link:'/shop/shop-1600'},
+    //       {title:'Collections list',link:'/shop/shop-list'},
+    //       {title:'Hidden search',link:'/shop'},
+    //       {title:'Search Full screen',link:'/shop'},
+    //     ]
+    //   },
+    //   {
+    //     link:'/shop',
+    //     title:'Hover Style',
+    //     list_menus:[
+    //       {title:'Hover Style 1',link:'/shop'},
+    //       {title:'Hover Style 2',link:'/shop'},
+    //       {title:'Hover Style 3',link:'/shop'},
+    //       {title:'Hover Style 4',link:'/shop'}
+    //     ]
+    //   },
+    // ]
   },
   {
     id:3,
     link:'/shop',
     title:'Ürünler',
-    mega_menu:true,
-    product_menus:[
-      {
-        id:1,
-        title:'Ticaret Sayfaları',
-        link:'/shop',
-        dropdown_menus:[
-          {title:'Only Categories',link:'/shop/shop-category'},
-          {title:'Shop Grid with Sidebar',link:'/shop/shop-filter-offcanvas'},
-          {title:'Shop Grid',link:'/shop'},
-          {title:'Categories',link:'/shop/shop-category'},
-          {title:'Shop List',link:'/shop/shop-list'},
-          {title:'Product Details',link:'/shop/shop-details'},
-        ]
-      },
-      {
-        id:2,
-        title:'Ürünler',
-        link:'/shop',
-        dropdown_menus:[
-          {title:'Product Simple',link:'/shop/shop-details'},
-          {title:'With Video',link:'/shop/shop-details-with-video'},
-          {title:'With Countdown Timer',link:'/shop/shop-details-with-countdown'},
-          {title:'Variations Swatches',link:'/shop/shop-details'},
-          {title:'List View',link:'/shop/shop-details-list'},
-          {title:'Details Gallery',link:'/shop/shop-details-gallery'}
-        ]
-      },
-      {
-        id:3,
-        title:'E-Ticaret',
-        link:'/shop',
-        dropdown_menus:[
-          {title:'Shopping Cart',link:'/shop/cart'},
-          {title:'Track Your Order',link:'/shop/order'},
-          {title:'Compare',link:'/shop/compare'},
-          {title:'Wishlist',link:'/shop/wishlist'},
-          {title:'Checkout',link:'/pages/checkout'},
-          {title:'My account',link:'/pages/profile'}
-        ]
-      },
-      {
-        id:4,
-        title:'Daha Fazla Sayfa',
-        link:'/shop',
-        dropdown_menus:[
-          {title:'About',link:'/pages/about'},
-          {title:'Login',link:'/pages/login'},
-          {title:'Register',link:'/pages/register'},
-          {title:'Forgot Password',link:'/pages/forgot'},
-          {title:'404 Error',link:'/pages/404'}
-        ]
-      },
-    ]
+    mega_menu:false,
+    // product_menus:[
+    //   {
+    //     id:1,
+    //     title:'Ticaret Sayfaları',
+    //     link:'/shop',
+    //     dropdown_menus:[
+    //       {title:'Only Categories',link:'/shop/shop-category'},
+    //       {title:'Shop Grid with Sidebar',link:'/shop/shop-filter-offcanvas'},
+    //       {title:'Shop Grid',link:'/shop'},
+    //       {title:'Categories',link:'/shop/shop-category'},
+    //       {title:'Shop List',link:'/shop/shop-list'},
+    //       {title:'Product Details',link:'/shop/shop-details'},
+    //     ]
+    //   },
+    //   {
+    //     id:2,
+    //     title:'Ürünler',
+    //     link:'/shop',
+    //     dropdown_menus:[
+    //       {title:'Product Simple',link:'/shop/shop-details'},
+    //       {title:'With Video',link:'/shop/shop-details-with-video'},
+    //       {title:'With Countdown Timer',link:'/shop/shop-details-with-countdown'},
+    //       {title:'Variations Swatches',link:'/shop/shop-details'},
+    //       {title:'List View',link:'/shop/shop-details-list'},
+    //       {title:'Details Gallery',link:'/shop/shop-details-gallery'}
+    //     ]
+    //   },
+    //   {
+    //     id:3,
+    //     title:'E-Ticaret',
+    //     link:'/shop',
+    //     dropdown_menus:[
+    //       {title:'Shopping Cart',link:'/shop/cart'},
+    //       {title:'Track Your Order',link:'/shop/order'},
+    //       {title:'Compare',link:'/shop/compare'},
+    //       {title:'Wishlist',link:'/shop/wishlist'},
+    //       {title:'Checkout',link:'/pages/checkout'},
+    //       {title:'My account',link:'/pages/profile'}
+    //     ]
+    //   },
+    //   {
+    //     id:4,
+    //     title:'Daha Fazla Sayfa',
+    //     link:'/shop',
+    //     dropdown_menus:[
+    //       {title:'About',link:'/pages/about'},
+    //       {title:'Login',link:'/pages/login'},
+    //       {title:'Register',link:'/pages/register'},
+    //       {title:'Forgot Password',link:'/pages/forgot'},
+    //       {title:'404 Error',link:'/pages/404'}
+    //     ]
+    //   },
+    // ]
   },
   {
     id:4,
