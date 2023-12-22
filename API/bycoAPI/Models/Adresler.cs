b2680f3421f42b1a7dc6cc314c365f47e90cfd6e
Kadir Berk Yağar <ykadirberk2@gmail.com>
2023-12-22 15:13:47 +0300

    added address

---
+++ API/bycoAPI/Models/Adresler.cs
@@ -0,0 +1,11 @@
+using System.Numerics;
+
+namespace bycoAPI.Models
+{
+    public class Adresler
+    {
+        public int adres_id {  get; set; }
+        public int user_id { get; set; }
+        public string? adres { get; set; }
+    }
+}
