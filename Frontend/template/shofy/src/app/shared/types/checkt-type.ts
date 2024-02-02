93d283d9fe77037bb2241bdb54bff65f216a0727
Samet CAN <cansamet1223@gmail.com>
2024-02-02 22:38:14 +0300

    Checkout ilerleme

--- Frontend/template/shofy/src/app/shared/types/checkt-type.ts
+++ Frontend/template/shofy/src/app/shared/types/checkt-type.ts
@@ -1,15 +1,10 @@
 export interface checkk{
     isim?:string;
-    soyisim?:string;
-    sirket_adi?:string;
+    vkn?:string;
     ulke?:string;
-    adres_satiri?:string;
-    il_ilce?:string;
-    kita?:string;
-    posta_kodu?:string;
+    adres_satiri?:string
     telefon?:string;
     email?:string;
-    siparis_notu?:string;
 
     satilan_urunler?:string[]
 }
