4021bf5180cdabf6cffeae0854a95db55de426e9
Ahmet Abdullah Gültekin <ahabgu@gmail.com>
2024-09-19 12:05:19 +0300

    Hash has changed.

--- Frontend/template/shofy/src/app/app-routing.module.ts
+++ Frontend/template/shofy/src/app/app-routing.module.ts
@@ -28,7 +28,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,{
-    useHash: true,
+    useHash: false,
     anchorScrolling: 'enabled',
     scrollPositionRestoration: 'enabled'
   })],
