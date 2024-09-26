6e83e29b2fa7b4aaf4f0fb9c1d84e533b0495684
Samet CAN <cansamet1223@gmail.com>
2024-09-26 13:22:16 +0300

    PaymentInfo added

---
+++ API/bycoAPI/Models/PaymentInfo.cs
@@ -0,0 +1,20 @@
+﻿namespace bycoAPI.Models
+{
+    public class PaymentInfo
+    {
+        public string? OrderId { get; set; }
+        public string? SuccessUrl { get; set; }
+        public string? ErrorUrl { get; set; }
+        public string? CustomerEmailAddress { get; set; }
+        public string? CustomerIpAddress { get; set; }
+        public ulong TxnAmount { get; set; }
+        public string? TxnType { get; set; }
+        public int TxnCurrencyCode { get; set; }
+        public int TxnInstallmentCount { get; set; }
+        public string? CardHolderName { get; set; }
+        public string? CardNumber { get; set; }
+        public string? CardExpireDateMonth { get; set; }
+        public string? CardExpireDateYear { get; set; }
+        public string? CardCvv2 { get; set; }
+    }
+}
--- Frontend/template/shofy/src/app/shared/services/url.ts
+++ Frontend/template/shofy/src/app/shared/services/url.ts
@@ -1,6 +1,6 @@
 export class URL {
     public geturl(): string {
-         return "http://localhost:5141/api";
+         return "https://localhost:44313/api";
         //return "https://bycobackend.online:5001/api";
     }
    constructor() { }
