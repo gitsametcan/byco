59606e08450c5f1f5e76d7e23a7e9bbc02d1acf0
Emirhan Erdoğan <emrhnerdogan01@gmail.com>
2024-11-08 08:53:26 +0300

    Alışverişi Tamamla method update

--- Frontend/template/shofy/src/app/shared/components/offcanvas/cart-sidebar/cart-sidebar.component.ts
+++ Frontend/template/shofy/src/app/shared/components/offcanvas/cart-sidebar/cart-sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CartService } from '@/shared/services/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart-sidebar',
@@ -7,6 +8,55 @@ import { CartService } from '@/shared/services/cart.service';
   styleUrls: ['./cart-sidebar.component.scss']
 })
 export class CartSidebarComponent {
+  showPopup = false;
 
-  constructor(public cartService:CartService){}
+  constructor(public cartService:CartService, private router: Router){}
+  checkTokenAndRedirect() {
+    const token = this.getCookie("session_key");
+
+    if (token) {
+      // Token varsa, checkout sayfasına yönlendir
+      this.router.navigate(['/pages/checkout']);
+    } else {
+      // Token yoksa, popup'ı göster
+      this.showPopup = true;
+    }
+  }
+  getTokenFromCookies(): string | null {
+    // Cookie'den token'ı al, burada basit bir örnekle çerez okuma işlemi var
+    const name = 'token=';
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return null;
+  }
+  redirectToLogin() {
+    // Üye Ol butonuna basıldığında login sayfasına yönlendirme
+    this.showPopup = false;
+    this.router.navigate(['/pages/login']);
+  }
+
+  continueWithoutAccount() {
+    // Üye Olmadan Devam Et butonuna basıldığında checkout sayfasına yönlendirme
+    this.showPopup = false;
+    this.router.navigate(['/pages/checkout']);
+  }
+  getCookie(name: string) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
 }
--- Frontend/template/shofy/src/app/shop/pages/cart/cart.component.ts
+++ Frontend/template/shofy/src/app/shop/pages/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { CartService } from '@/shared/services/cart.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -7,11 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent {
+  showPopup = false;
 
   couponCode: string = '';
   shipCost: number = 100; // Kargo ücreti varsayılan olarak 100 TL
 
-  constructor (public cartService:CartService) {}
+  constructor (public cartService:CartService
+    , private router: Router
+  ) {}
 
   handleCouponSubmit() {
     console.log(this.couponCode);
@@ -30,5 +34,54 @@ export class CartComponent {
     } else {
       this.shipCost = value as number;
     }
+  }
+
+  checkTokenAndRedirect() {
+    const token = this.getCookie("session_key");
+
+    if (token) {
+      // Token varsa, checkout sayfasına yönlendir
+      this.router.navigate(['/pages/checkout']);
+    } else {
+      // Token yoksa, popup'ı göster
+      this.showPopup = true;
+    }
+  }
+  getTokenFromCookies(): string | null {
+    // Cookie'den token'ı al, burada basit bir örnekle çerez okuma işlemi var
+    const name = 'token=';
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) === 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return null;
+  }
+  redirectToLogin() {
+    // Üye Ol butonuna basıldığında login sayfasına yönlendirme
+    this.showPopup = false;
+    this.router.navigate(['/pages/login']);
+  }
+
+  continueWithoutAccount() {
+    // Üye Olmadan Devam Et butonuna basıldığında checkout sayfasına yönlendirme
+    this.showPopup = false;
+    this.router.navigate(['/pages/checkout']);
+  }
+  getCookie(name: string) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
   }
 }
